c898a2f008eebc8b905dc5c64fae8b4f76291c86
Sofia <pachecosofia277@gmail.com>
2026-04-14 18:36:39 -0500

    UPD: App file

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        // FOR NORMAL
+        int[] array = new int[]{1,2,3,4,5};
+        System.out.println(array[0]);
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        } 
+
+        // FOR EACH 
+        for (int i : array){
+            System.out.println(i);
+        }
+
+        
+        
     }
 }
